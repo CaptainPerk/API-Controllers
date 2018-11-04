06a7fc51187dfeaad91dd2dc6b3cbdb163c54f11
Jon Perkins <captainperk81@gmail.com>
2018-11-04 08:57:00 -0500

    Receiving Different Data Formats

--- ApiControllers/Controllers/ContentController.cs
+++ ApiControllers/Controllers/ContentController.cs
@@ -12,5 +12,21 @@ namespace ApiControllers.Controllers
         [HttpGet("object/{format?}")]
         [FormatFilter]
         public Reservation GetObject() => new Reservation {ReservationId = 100, ClientName = "Joe", Location = "Board Room"};
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public Reservation ReceiveJson([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Json";
+            return reservation;
+        }
+
+        [HttpPost]
+        [Consumes("application/xml")]
+        public Reservation ReceiveXml([FromBody] Reservation reservation)
+        {
+            reservation.ClientName = "Xml";
+            return reservation;
+        }
     }
 }
